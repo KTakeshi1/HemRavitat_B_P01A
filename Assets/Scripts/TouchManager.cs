e18be40ea83856d7ee5f6524b03180b10b246b98
KTakeshi1 <takeshik623@gmail.com>
2024-02-17 00:00:46 -0600

    Add Main Menu

---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.Device;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
